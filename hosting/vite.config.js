356fa2ecd59fb0362866caa9a8491e6db25db3c9
Alexander Horner <mail@alexanderhorner.com>
2022-07-22 00:35:15 +0200

    Add footer

--- hosting/vite.config.js
+++ hosting/vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ['AOS'],
-          },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        credits: resolve(__dirname, 'credits.html')
+      },
+      output: {
+        manualChunks: {
+          vendor: ['AOS'],
         },
       },
     },
+  },
 })
